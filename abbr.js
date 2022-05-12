cfcda1d11b1bb0a00e70ef6e392fcb7f03e1ac3f
Xuanwo <github@xuanwo.io>
2022-05-12 08:38:11 +0800

    fix: Map doesn't have map function (#7)
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- abbr.js
+++ abbr.js
@@ -2,4 +2,9 @@ import {create_replies} from "./utils"
 import {replies} from "./entries";
 
 // Use `full` as title, and `abbr` as real body.
-create_replies(replies.map(([title, body]) => [body, title]));
+let abbrReplies = new Map();
+for (const [key, value] of replies) {
+    abbrReplies[value] = key;
+}
+
+create_replies(abbrReplies);
